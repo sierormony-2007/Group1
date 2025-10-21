640149f4b2e911c8ce662cad72f42514c900a69d
sean22-blip <iseandxd@gmail.com>
2025-10-21 15:12:41 +0700

    daff

--- csll.hpp
+++ csll.hpp
@@ -1,1 +1,9 @@
-#include<iostream>
+void csll_observe(Csll* obj, void (Csll::*method)(), string msg){
+    auto t0 = clk::now();
+
+    (obj->*method)(); // perform operation
+
+    auto t1 = clk::now();
+
+    auto duration = chrono::duration_cast<chrono::nanoseconds>(t1 - t0);
+    cout<<msg <<": "<<duration.count() <<" nanosecond(s)" <<endl;
--- main.cpp
+++ main.cpp
@@ -6,17 +6,8 @@
 #include "csll.hpp"
 using namespace std;
 int main(){
+csll_observe();
 
-void sll_observe(Sll* obj, void (Sll::*method)(), string msg){
-    auto t0 = clk::now();
 
-    (obj->*method)(); // perform operation
-
-    auto t1 = clk::now();
-
-    auto duration = chrono::duration_cast<chrono::nanoseconds>(t1 - t0);
-    cout<<msg <<": "<<duration.count() <<" nanosecond(s)" <<endl;
     return 0;
-}
-
 }
