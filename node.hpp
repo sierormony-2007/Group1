e730df20cb7b5e999f717220a1ec2273d27d44bd
Chhairady <Chhairady08@gmail.com>
2025-10-21 15:49:23 +0700

    node

--- node.hpp
+++ node.hpp
@@ -1,1 +1,17 @@
 #include<iostream>
+#ifndef Node
+#define Node
+using namespace std;
+
+struct SNode{
+    int data;
+    SNode* next;
+};
+
+struct DNode{
+   int value;
+   DNode* next;
+   DNode* prev;
+};
+
+#endif
