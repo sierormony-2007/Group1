ff3c0f816d120f73fd5b21ddc646f899936182d5
Sie Rormony <niin2015@gmail.com>
2025-10-21 15:55:56 +0700

    abc

--- dll.hpp
+++ dll.hpp
@@ -1,1 +1,23 @@
 #include<iostream>
+#include<string>
+using namespace std;
+struct DNode{
+    int value;
+    DNode* next;
+    DNode* prev;
+};
+class Doublylinkedlist{
+    private:
+       DNode *head, *tail;
+       int n;
+    public:
+    Doublylinkedlist(){
+        head = nullptr;
+        tail = nullptr;
+        n=0;
+    }
+    void deleteAt(int val){
+       DNode* temp = head;
+         
+    }
+};
